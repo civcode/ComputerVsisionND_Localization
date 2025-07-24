32387dab3ebfa3bd65f1c29d52d94833ec5b5722
chris <ivi3001@yahoo.de>
2025-07-24 17:50:23 +0200

    solved 2d histogram exercise

--- 4_3_2D_Histogram_Filter/localizer.py
+++ 4_3_2D_Histogram_Filter/localizer.py
@@ -16,10 +16,14 @@ def initialize_beliefs(grid):
 
 def sense(color, grid, beliefs, p_hit, p_miss):
     new_beliefs = []
-
-    #
-    # TODO - implement this in part 2
-    #
+    for g_row, b_row in zip(grid, beliefs):
+        new_b_row = []
+        for g, b in zip(g_row, b_row):
+            if g == color:
+                new_b_row.append(b * p_hit)
+            else:
+                new_b_row.append(b * p_miss)
+        new_beliefs.append(new_b_row)
 
     return new_beliefs
 
@@ -29,8 +33,8 @@ def move(dy, dx, beliefs, blurring):
     new_G = [[0.0 for i in range(width)] for j in range(height)]
     for i, row in enumerate(beliefs):
         for j, cell in enumerate(row):
-            new_i = (i + dy ) % width
-            new_j = (j + dx ) % height
+            new_i = (i + dy ) % height
+            new_j = (j + dx ) % width
             # pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
     return blur(new_G, blurring)
